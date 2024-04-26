4e0510d72ec7713df7b88cacd07845a7b27cdef8
Kid A <me@thisismeamir.com>
2024-04-26 15:39:42 +0330

    [Finalize Commit] WebVersion 0.1 is complete and on the githubpages.io

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,13 @@ import TheFooter from "./components/TheFooter.vue";
 
 </script>
 <style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
 
-
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
